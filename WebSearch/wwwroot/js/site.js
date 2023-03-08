b2155a6267af64ab930950eb1c9cd0c6b39bcfb0
Rolf Geschke <71587437+Natorden@users.noreply.github.com>
2023-03-08 16:38:07 +0100

    some ui added, needs to display and search

--- WebSearch/wwwroot/js/site.js
+++ WebSearch/wwwroot/js/site.js
@@ -1,9 +1,32 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-let ViewModel = function (){
+// Write your JavaScript code
+
+var ViewModel = function() {
+    console.log("View Model started")
     
-    let ws = this;
-    ws.searchTerms = ko.observable();
+    let me =  this;
+    
+    me.userQuery = ko.observable();
+    me.returnedQuery = ko.observableArray();
+    me.queryMatches = ko.observable();
+    me.queryTime = ko.observable();
+    
+    me.search = function (){
+        $.ajax({
+            url: "http:localhost:8889/Search?terms=" + me.userQuery() + "&numberOfResults=10",
+            success: function (data) {
+                me.queryMatches(data.documents.length);
+                me.queryTime(data.elapsedMilliseconds);
+                me.returnedQuery.removeAll();
+                data.documents.forEach( function (hit) {
+                    me.returnedQuery.push(queryMatches);
+                });
+            }
+            });
+    }
+
+    ko.applyBindings(new ViewModel());
+    
 }
