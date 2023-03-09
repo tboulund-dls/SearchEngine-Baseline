9d9eaa7f7593175f4cec1074b3e982c5c9d78679
tomaspeniak <71640446+pennyb0i@users.noreply.github.com>
2023-03-09 12:30:59 +0100

    lol

--- LoadBalancer/Controllers/LoadBalancerController.cs
+++ LoadBalancer/Controllers/LoadBalancerController.cs
@@ -8,10 +8,11 @@ namespace LoadBalancer.Controllers {
     public class LoadBalancerController : Controller {
         private readonly ILoadBalancer _loadBalancer = new LoadBalancer.LoadBalancer(new BasicStrategy());
 
-        public LoadBalancerController()
-    {
-        _loadBalancer.SetActiveStrategy(new BasicStrategy());
-    }[HttpPost]
+        public LoadBalancerController() {
+            _loadBalancer.SetActiveStrategy(new BasicStrategy());
+        }
+        
+        [HttpPost]
         public void Create(string url) {
             _loadBalancer.AddService(url);
         }
